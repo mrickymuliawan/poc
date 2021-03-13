50e4bc26ceac48793634c23fee380f2f41c9bb5a
Ricky Muli <mrickymuliawan@gmail.com>
2021-03-13 21:27:30 +0700

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: process.env.ORIGIN_URL,
+  },
+});
+
+io.on('connection', (socket) => {
+  if (process.env.APP_ENV == 'dev') {
+    console.log('user connected');
+  }
+  socket.on('send-message', (payload) => {
+    io.emit('receive-message', payload)
+  })
+  socket.on('disconnect', () => {
+    if (process.env.APP_ENV == 'dev') {
+      console.log('user disconnected');
+    }
+  })
+});
+
+http.listen(process.env.PORT, () => {
+  console.log('socket running on port '+ process.env.PORT);
+});
