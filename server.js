15e36ee78291e30a5cc4934964da2b0b04d7d878
rickyhehe10 <dev.rickymuli@gmail.com>
2024-04-04 01:03:31 +0700

    adjust

--- server.js
+++ server.js
@@ -3,14 +3,35 @@ var io = require('socket.io-client');
 var socket = io.connect(process.env.SOCKET_SERVER_HOST, { reconnect: true });
 const rtsp = require('rtsp-ffmpeg');
 const fs = require('fs');
+// const sharp = require('sharp');
+var Jimp = require("jimp");
+
 var stream = new rtsp.FFMpeg({
   input: process.env.CAMERA_HOST,
-  rate: 1, // output framerate (optional)
+  rate: 3, // output framerate (optional)
   resolution: '640x480', // output resolution in WxH format (optional)
-  // quality: 3,
+  quality: 1,
 });
 
 var pipeStream = function (data) {
+  // Jimp.read(data).then(image => {
+  //   const x = image
+  //     .resize(640, 320) // resize
+  //     .quality(100) // set JPEG quality
+  //     .brightness(0.1) // set greyscale
+  //     // .contrast(0.1) // set greyscale
+  //     .write("jimp.jpg")
+  //     .getBase64(Jimp.AUTO, function (err, src) {
+  //       const json = {
+  //         image: src,
+  //         outletId: 35,
+  //       }
+  //       socket.emit('stream-pic', JSON.stringify(json));
+  //     })
+
+  // })
+  // sharp(data).rotate().toFile('output.jpg', (err, info) => console.log(info))
+
   const base64String = 'data:image/png;base64,' + data.toString('base64')
   let base64Image = base64String.split(';base64,').pop();
 
@@ -20,6 +41,7 @@ var pipeStream = function (data) {
   const json = {
     image: base64String,
     outletId: 35,
+    camType: 2
   }
   socket.emit('stream-pic', JSON.stringify(json));
 };
